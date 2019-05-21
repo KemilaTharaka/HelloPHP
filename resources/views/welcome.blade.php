44c510db46f8560cf8e37831f031eaef774de8f5
KemilaTharaka <kemiladesilva@gmail.com>
2019-05-21 20:58:09 +0530

    minar change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Hello Laravel</title>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
